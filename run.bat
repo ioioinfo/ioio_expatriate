861321b405054cf4027f61df1a407b1bea8b4ae7
oopfuture <oopfuture@gmail.com>
2017-07-06 15:42:14 +0800

    init

---
+++ run.bat
@@ -0,0 +1,5 @@
+@echo off
+title ioio expatriate 18022
+cd /d %~dp0
+nodemon .
+@echo on
---
+++ server/controller/main_controller.js
@@ -0,0 +1,55 @@
+/**
+ ┌──────────────────────────────────────────────────────────────┐
+ │               ___ ___ ___ ___ ___ _  _ ___ ___               │
+ │              |_ _/ _ \_ _/ _ \_ _| \| | __/ _ \              │
+ │               | | (_) | | (_) | || .` | _| (_) |             │
+ │              |___\___/___\___/___|_|\_|_| \___/              │
+ │                                                              │
+ │                                                              │
+ │                       set up in 2015.2                       │
+ │                                                              │
+ │   committed to the intelligent transformation of the world   │
+ │                                                              │
+ └──────────────────────────────────────────────────────────────┘
+*/
+
+var _ = require('lodash');
+var r = require('request');
+var moment = require('moment');
+var eventproxy = require('eventproxy');
+
+var moduel_prefix = 'ioio_expatriate_main';
+
+exports.register = function(server, options, next) {
+    var wx_api = server.plugins.services.wx_api;
+    var person = server.plugins.services.person;
+
+    var cookie_options = {ttl:10*365*24*60*60*1000};
+    var cookie_key = "ioio_expatriate_cookie";
+
+    server.route([
+        //首页
+        {
+            method: 'GET',
+            path: '/index',
+            handler: function(request, reply) {
+                return reply.view("index");
+            },
+        },
+
+        {
+            method: 'GET',
+            path: '/test',
+            handler: function(request, reply) {
+                return reply("ioio");
+            },
+        },
+
+    ]);
+
+    next();
+}
+
+exports.register.attributes = {
+    name: moduel_prefix
+};
